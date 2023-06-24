3a60e09fac89f79ac41a794e6bb198e181a562dd
Riccardo Penco <136708107+errepuntopenco@users.noreply.github.com>
2023-06-24 16:54:26 -0400

    Replace "%3A" with ":" in searchText

--- inspire-search/src/index.tsx
+++ inspire-search/src/index.tsx
@@ -15,7 +15,7 @@ export default function Command() {
   });
   const showCitations = (item) => {
     return () => {
-      setSearchText(`refersto%3Arecid%3A${item.id}`);
+      setSearchText(`refersto:recid:${item.id}`);
     }
   };
 
@@ -35,35 +35,35 @@ export default function Command() {
           key={item.id}
           title={`${index + 1 + indexOffset}. ${item.metadata.titles[0].title}`}
           subtitle={abbreviateNames(item.metadata.authors)}
-      accessories={[{ text: `${item.metadata.citation_count}` }, { text: `(${item.created.slice(0, 4)})` }]}
-      actions={
-        <ActionPanel title="Inspire HEP Search">
-          <Action
-            title="Show citations"
-            shortcut={{ modifiers: ["cmd"], key: "]" }}
-            icon={Icon.ArrowRightCircle}
-            onAction={showCitations(item)}
-          />
-          <ActionPanel.Section title="Navigation">
-            <Action
-              title="Next Page"
-              shortcut={{ modifiers: ["cmd"], key: "arrowRight" }}
-              onAction={() => {
-                setPageNumber(pageNumber + 1);
-              }}
-            />
-            <Action
-              title="Previous Page"
-              shortcut={{ modifiers: ["cmd"], key: "arrowLeft" }}
-              onAction={() => {
-                if (pageNumber > 1) {
-                  setPageNumber(pageNumber - 1);
-                }
-              }}
-            />
-          </ActionPanel.Section>
-        </ActionPanel>
-      }
+          accessories={[{ text: `${item.metadata.citation_count}` }, { text: `(${item.created.slice(0, 4)})` }]}
+          actions={
+            <ActionPanel title="Inspire HEP Search">
+              <Action
+                title="Show citations"
+                shortcut={{ modifiers: ["cmd"], key: "]" }}
+                icon={Icon.ArrowRightCircle}
+                onAction={showCitations(item)}
+              />
+              <ActionPanel.Section title="Navigation">
+                <Action
+                  title="Next Page"
+                  shortcut={{ modifiers: ["cmd"], key: "arrowRight" }}
+                  onAction={() => {
+                    setPageNumber(pageNumber + 1);
+                  }}
+                />
+                <Action
+                  title="Previous Page"
+                  shortcut={{ modifiers: ["cmd"], key: "arrowLeft" }}
+                  onAction={() => {
+                    if (pageNumber > 1) {
+                      setPageNumber(pageNumber - 1);
+                    }
+                  }}
+                />
+              </ActionPanel.Section>
+            </ActionPanel>
+          }
         />
       ))}
     </List>
