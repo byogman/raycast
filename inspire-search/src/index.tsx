bd23a0c324b73d9ceb729d53549176c04fbe2ef6
Riccardo Penco <136708107+errepuntopenco@users.noreply.github.com>
2023-06-25 09:38:11 -0400

    Prevent Next Page action from going past last page

--- inspire-search/src/index.tsx
+++ inspire-search/src/index.tsx
@@ -9,6 +9,7 @@ export default function Command() {
   const [searchText, setSearchText] = useState("");
   const [pageNumber, setPageNumber] = useState(1);
   const [indexOffset, setIndexOffset] = useState(0);
+  const [resultsNumber, setResultsNumber] = useState(0);
   const { isLoading, data } = useFetch(`${API_PATH}&sort=mostrecent&page=${pageNumber}&q=${searchText}`, {
     // to make sure the screen isn't flickering when the searchText changes
     keepPreviousData: true,
@@ -35,6 +36,10 @@ export default function Command() {
     setIndexOffset((pageNumber - 1) * 9);
   }, [pageNumber]);
 
+  useEffect(() => {
+    setResultsNumber(data.hits.total);
+  }, [data]);
+
   return (
     <List isLoading={isLoading} searchBarPlaceholder={`Search InspireHEP...`} searchText={searchText} onSearchTextChange={setSearchText} throttle>
       {(searchText && data && data.hits && Array.isArray(data.hits.hits) ? data.hits.hits : []).map((item, index) => (
@@ -58,14 +63,16 @@ export default function Command() {
                 onAction={showReferences(item)}
               />
               <ActionPanel.Section title="Navigation">
-                <Action
-                  title="Next Page"
-                  shortcut={{ modifiers: ["cmd"], key: "arrowRight" }}
-                  icon={Icon.ChevronRight}
-                  onAction={() => {
-                    setPageNumber(pageNumber + 1);
-                  }}
-                />
+                  <Action
+                    title="Next Page"
+                    shortcut={{ modifiers: ["cmd"], key: "arrowRight" }}
+                    icon={Icon.ChevronRight}
+                    onAction={() => {
+                      if (pageNumber < Math.ceil(resultsNumber / 9)) {
+                      setPageNumber(pageNumber + 1);
+                      }
+                    }}
+                  />
                 <Action
                   title="Previous Page"
                   shortcut={{ modifiers: ["cmd"], key: "arrowLeft" }}
--- inspire-search/test.js
+++ inspire-search/test.js
@@ -14,21 +14,21 @@ let n = 6;
 fetchInspireHepData("refersto:recid:1367306")
 	.then(data => {
 		results = data;
-		// console.log(results);
    	})
   	.catch(error => console.error(error));
 	
 setTimeout(function(){
-	console.log(results.hits.hits[n]);
-	console.log(results.hits.hits[n].metadata.titles[0].title);
-	// console.log(results.hits.hits[n].metadata.arxiv_eprints[0].value);
-    // console.log(results.hits.hits[n].metadata.authors.map(obj => obj.full_name).join(', '));
-    console.log(results.hits.hits[n].metadata.authors);
-    console.log(results.hits.hits[n].metadata.collaborations.map(obj => obj.value).join(', '));
-    console.log(results.hits.hits[n].metadata.citation_count);
-    console.log(results.hits.hits[n].created.slice(0,4));
-    console.log(results.hits.hits[n].links.bibtex);
-    console.log(results.hits.hits[n].id);
-    console.log(results.hits.hits[n].links.citations);
+  console.log(results.hits.total);
+	// console.log(results.hits.hits[n]);
+	// console.log(results.hits.hits[n].metadata.titles[0].title);
+	// // console.log(results.hits.hits[n].metadata.arxiv_eprints[0].value);
+  //   // console.log(results.hits.hits[n].metadata.authors.map(obj => obj.full_name).join(', '));
+  //   console.log(results.hits.hits[n].metadata.authors);
+  //   console.log(results.hits.hits[n].metadata.collaborations.map(obj => obj.value).join(', '));
+  //   console.log(results.hits.hits[n].metadata.citation_count);
+  //   console.log(results.hits.hits[n].created.slice(0,4));
+  //   console.log(results.hits.hits[n].links.bibtex);
+  //   console.log(results.hits.hits[n].id);
+  //   console.log(results.hits.hits[n].links.citations);
 }, 3000);
 
